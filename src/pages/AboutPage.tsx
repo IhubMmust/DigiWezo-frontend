abc05b53c068f37475509a273e6b6c45ee32b73e
Muchael123 <mainamichael2849@gmail.com>
2025-03-26 15:03:27 +0300

    Add about us page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,16 +1,12 @@
+import About from "../components/about/About";
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
 
 function AboutPage() {
   return (
-    <div className="bg-gray-900 text-white h-screen  flex flex-col justify-center items-center">
+    <div className="flex text-white flex-col">
       <Header/>
-      <p className="text-green-500 text-2xl">Hello @BinaryBrigades</p>
-      {/* 1 programic joke */}
-      <p className="text-red-500 text-2xl">
-        Why do programmers prefer dark mode?
-      </p>
-      <p className="text-blue-500 text-2xl">Because light attracts bugs.</p>
+      <About />
       <Footer/>
     </div>
   );
